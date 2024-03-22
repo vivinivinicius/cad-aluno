d9afd6f6aff8a0b9feb9e4c5dfbc541e440fba77
Vinicius <akazanatogamez@gmail.com>
2024-03-22 13:52:36 -0300

    folder aluno created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
